aed14bdcc675cb983c57df44990c540dd535772b
Islam <jhosepchai@gmail.com>
2019-06-11 11:21:04 +0700

    Nuevo Adaptador

--- app/src/main/java/admin/lab/app/utec/com/conteos/Adapters/ClasesInfoAdapter.java
+++ app/src/main/java/admin/lab/app/utec/com/conteos/Adapters/ClasesInfoAdapter.java
@@ -2,11 +2,12 @@ package admin.lab.app.utec.com.conteos.Adapters;
 
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import java.util.ArrayList;
-import java.util.List;
+
 
 public class ClasesInfoAdapter extends RecyclerView.Adapter<ClasesInfoAdapter.ViewHolder> {
 
@@ -17,16 +18,27 @@ public class ClasesInfoAdapter extends RecyclerView.Adapter<ClasesInfoAdapter.Vi
 
     public ClasesInfoAdapter(ArrayList Materia,ArrayList Aula,ArrayList Docente,ArrayList Horas,
                              ArrayList Dias,ArrayList Seccion,ArrayList Id_clase,ArrayList Inscritos,
-                             ArrayList cantiadad_contada,int layout , ConteosAdapter.OnItemClickListener listener){
+                             ArrayList cantiadad_contada,int layout , ConteosAdapter.OnItemClickListener itemClickListener){
 
-
-
+        this.Materia = Materia;
+        this.Aula = Aula;
+        this.Docente= Docente;
+        this.Horas = Horas;
+        this.Dias = Dias;
+        this.Seccion = Seccion;
+        this.Id_clase = Id_clase;
+        this.Inscritos = Inscritos;
+        this.cantiadad_contada = cantiadad_contada;
+        this.layout = layout;
+        this.itemClickListener = itemClickListener;
     }
 
     @NonNull
     @Override
     public ClasesInfoAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return null;
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(layout,viewGroup,false);
+        ClasesInfoAdapter.ViewHolder viewHolder = new ClasesInfoAdapter.ViewHolder(view);
+        return viewHolder;
     }
 
     @Override
@@ -36,12 +48,24 @@ public class ClasesInfoAdapter extends RecyclerView.Adapter<ClasesInfoAdapter.Vi
 
     @Override
     public int getItemCount() {
-        return 0;
+
+        try {
+            return Materia.size();
+        }catch (Exception ex){
+            return  0;
+        }
     }
 
     public static  class ViewHolder extends RecyclerView.ViewHolder {
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+
         }
+
+        public  void  bind(int id,String Materi, String Docente){}
+    }
+
+    public interface OnItemClickListener{
+        void  OnItemClick(int id);
     }
 }
