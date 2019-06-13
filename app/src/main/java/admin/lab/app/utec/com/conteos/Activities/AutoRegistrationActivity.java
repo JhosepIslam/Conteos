c4514d730904a6d60a47b8c7b739990d74a8f4f8
JhosepIslam <jhosepchai@gmail.com>
2019-06-13 15:35:09 -0600

    Actualizacion

---
+++ app/src/main/java/admin/lab/app/utec/com/conteos/Activities/AutoRegistrationActivity.java
@@ -0,0 +1,15 @@
+package admin.lab.app.utec.com.conteos.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import admin.lab.app.utec.com.conteos.R;
+
+public class AutoRegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auto_registration);
+    }
+}
